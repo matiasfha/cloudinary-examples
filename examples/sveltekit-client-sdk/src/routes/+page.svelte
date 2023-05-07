859dccddcb5eca98e9b84126a3a7675d6c16f26f
Matias Hernandez <matiasfh@gmail.com>
2023-05-07 19:12:12 -0400

    fix typos

--- examples/sveltekit-client-sdk/src/routes/+page.svelte
+++ examples/sveltekit-client-sdk/src/routes/+page.svelte
@@ -1,16 +1,14 @@
 <script>
 	import { Cloudinary } from '@cloudinary/url-gen';
-	import { scale } from '@cloudinary/url-gen/actions/resize';
+
 	const cld = new Cloudinary({
 		cloud: {
 			cloudName: 'tutorials'
 		}
 	});
-	// Instantiate a CloudinaryImage object for a image with public ID
-	const myImage = cld.image('cld-sample-5');
-	// Perform transformations
-	myImage.format('auto').quality('auto');
 
+	const myImage = cld.image('cld-sample-5');
+	myImage.format('auto').quality('auto');
 </script>
 
 <main class="main">
@@ -21,7 +19,7 @@
 	<div class="container">
 		<div>
 			<h2>Auto format, Auto Quality</h2>
-			<img src={myImage.toURL()} alt="cloudinary image" width={300} />
+			<img src={myImage.toURL()} alt="from cloudinary" width={300} />
 		</div>
 	</div>
 
@@ -43,31 +41,11 @@
 		padding: 0 1em;
 		margin: 0 auto;
 		display: flex;
-		alig-items: center;
+		align-items: center;
 	}
 
 	.title {
 		text-align: center;
 		margin: 2em 0;
-	}
-
-	.subtitle {
-		font-weight: normal;
-		text-align: center;
-		margin: -1em 0 2em;
-	}
-
-	.images {
-		list-style: none;
-		padding: 0;
-		margin: 0;
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-		grid-gap: 3em;
-	}
-
-	.imageTitle {
-		font-size: 1.4em;
-		margin: 0.4em 0 0;
 	}
 </style>
